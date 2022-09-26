0de2f6ea06ddedc94a8f7ce23450d7f0a0e52672
AliefGr <github6754@gmail.com>
2022-09-26 17:08:49 -0600

    AliefGr

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+    menuNav.classList.toggle("menu-active");
+});
+
+// scrolling active
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener('scroll', () => {
+    
+    const windowPosition = window.scrollY > 0;
+    navBar.classList.toggle('scrolling-active', windowPosition);
+
+});
